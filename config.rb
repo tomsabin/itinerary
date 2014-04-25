619d18dbfc469c01ee44d1f5c8469d1de3776b82
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-04-25 16:40:01 +0100

    Move Compass config file to root of project. Add iOS meta tags

--- config.rb
+++ config.rb
@@ -2,9 +2,9 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "../public/images"
+css_dir = "client/stylesheets"
+sass_dir = "client/sass"
+images_dir = "public/images"
 # javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
